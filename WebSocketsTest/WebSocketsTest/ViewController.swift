9d7de9f153d8151a189973ae4c5e7e01b86a988d
Timur Nuriev <uzumaci@mail.ru>
2021-01-08 20:40:04 +0400

    created project with pods

---
+++ WebSocketsTest/WebSocketsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebSocketsTest
+//
+//  Created by Tim on 08.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
